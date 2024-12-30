9eee943e0324b4f7770aa17df549533a323f8bbf
zoe12378 <144758574+zoe12378@users.noreply.github.com>
2024-12-31 00:27:33 +0800

    12:27

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ from linebot.exceptions import LineBotApiError, InvalidSignatureError
 
 app = Flask(__name__)
 
-# 環境變數設定
-LINE_CHANNEL_SECRET = os.getenv('47250539de3b4c9b5f35c0c62ed15527')
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv('/ifIOXVIsQQPpwIukXv4QDZgA979eZSAC5G87k1JLKZnG0xSriHyC+xtydkAxVX0eKBXB/KyCdkMNx3oCm74xxjuxPFeKH8giQkrxaAP0XNNZG5wuxZ/66nfX0hqu9cj5WFofra4uEr0sbc6xXyyDgdB04t89/1O/w1cDnyilFU=')
+LINE_CHANNEL_SECRET = '47250539de3b4c9b5f35c0c62ed15527'
+LINE_CHANNEL_ACCESS_TOKEN = '/ifIOXVIsQQPpwIukXv4QDZgA979eZSAC5G87k1JLKZnG0xSriHyC+xtydkAxVX0eKBXB/KyCdkMNx3oCm74xxjuxPFeKH8giQkrxaAP0XNNZG5wuxZ/66nfX0hqu9cj5WFofra4uEr0sbc6xXyyDgdB04t89/1O/w1cDnyilFU='
+
+if not LINE_CHANNEL_SECRET or not LINE_CHANNEL_ACCESS_TOKEN:
+    print("LINE_CHANNEL_SECRET 或 LINE_CHANNEL_ACCESS_TOKEN 未設置，請確認環境變數是否正確。")
+    exit(1)
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
